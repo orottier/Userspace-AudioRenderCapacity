920105f9cdc9e6cbf8e21d1baa4579ea0fb8e38a
Otto <ottorottier@gmail.com>
2024-03-26 18:01:28 +0100

    Another try with just `new Date()` in the render thread

--- proc.js
+++ proc.js
@@ -17,14 +17,19 @@ class RenderCapacityProcessor extends AudioWorkletProcessor {
 
         // previous render quantum time (ms)
         this._prev_time = 0;
+        
+        // timestamps when starting the measurement
+        this._start_time = 0;
 
         // just to make sure we track some side effects so the compiler won't drop our load
         this._sum = 0.0;
 
         this.port.onmessage = event => {
-            this._sum = 0.0;
             this._state = event.data.state;
-            this._sab = new Int32Array(event.data.sab);
+            this._sum = 0.0;
+            this._prev_time = 0;
+            this._start_time = 0;
+            this._load_factor = 0;
         }
 
         this.port.onmessageerror = (e) => {
@@ -39,32 +44,49 @@ class RenderCapacityProcessor extends AudioWorkletProcessor {
             return true;
         }
 
+        var time = Date.now();
+        var duration = time - this._prev_time;
+
         if (this._state == 1) {
             this._max_flops *= 2;
-            var time = Atomics.load(this._sab, 0);
-            var duration = time - this._prev_time;
-            if (this._prev_time != 0 && duration > 128 * 2 * 1000000. / sampleRate) {
-                console.log("duration", duration);
-                console.log("time", time);
-                console.log("max_flops", this._max_flops);
-                console.log("done");
+
+            if (this._start_time == 0) {
+                this._start_time = time;
+            } else if (duration > 128 * 5 * 1000. / sampleRate) {
                 this._state = 0;
+                this.port.postMessage({"max_flops": this._max_flops});
                 return true;
             }
+
             this._prev_time = time;
-            if (time > 90000) {
-                console.log("limit");
+
+            if (time - this._start_time > 500) {
                 this._state = 0;
-            }
-            for (var i=0; i<this.max_flops; i++) {
-                //this._sum += Math.sin(Math.random());
+                this.port.postMessage({"limit": this._max_flops});
+            } else {
+                for (var i=0; i<this._max_flops; i++) {
+                    this._sum += Math.sin(Math.random());
+                }
             }
         } else if (this._state == 2) {
-            if (this._load_factor < 1.) {
+            if (this._start_time == 0) {
+                this._start_time = time;
+            } else if (duration > 128 * 5 * 1000. / sampleRate) {
+                this._state = 0;
+                this.port.postMessage({"load_factor": this._load_factor});
+                return true;
+            }
+
+            this._prev_time = time;
+
+            if (this._load_factor < 2.) {
                 this._load_factor += 0.01;
                 for (var i=0; i<this._max_flops * this._load_factor; i++) {
                     this._sum += Math.sin(Math.random());
                 }
+            } else {
+                this._state = 0;
+                this.port.postMessage({"measure_limit": this._load_factor});
             }
         }
 
